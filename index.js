82dec9f1348f40ccdd9072fa3f9e971098e1468b
Samantha Webb <samantha.webb2436@gmail.com>
2025-01-28 19:29:06 -0600

    This one wasn't too bad

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = [
+        "Milo",
+        "Otis",
+        "Garfield",
+    ];
+
+function destructivelyAppendCat(name) {
+    name = "Ralph";
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    name = "Bob";
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {;
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {;
+    cats.shift();
+};
+
+function appendCat(name) {;
+    name = "Broom";
+    const appendCat = [...cats, name];
+    return appendCat;
+};
+
+
+function prependCat(name) {;
+    name = "Arnold";
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+
+function removeLastCat() {
+   const removeLastCat = cats.slice(0, -1);
+    return removeLastCat;
+};
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice(1, 3);
+     return removeFirstCat;
+ };
